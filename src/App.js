cad83bc02c893aaa091448fceed3c6ca5b256bcb
prokharovaky <144281695+prokharovaky@users.noreply.github.com>
2024-10-23 23:50:46 +0300

    Firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import '../src/styles.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+function speenTheWheel()
+{
+    let randomDeg = Math.floor(Math.random() * 360);
+    let numOfSpins = Math.floor(Math.random() * 20) + 5;
+    const image = document.getElementById("SpImg");
+    const totalRotation = (numOfSpins * 360) + randomDeg;
+
+    let currentRotation = 0;  // Текущий угол поворота
+    const duration = 3000;    // Длительность анимации (3 секунды)
+    const intervalTime = 10;  // Частота обновления анимации
+    const steps = duration / intervalTime;  // Количество шагов анимации
+    const rotationPerStep = totalRotation / steps; 
+    
+    const interval = setInterval(() => {
+        currentRotation += rotationPerStep;
+        image.style.transform = `rotate(${currentRotation}deg)`;  
+
+        if (currentRotation >= totalRotation) {
+            clearInterval(interval);
+        }
+    }, intervalTime);
+
+    let points = document.getElementById("points");
+    let numberOfPoints = points.textContent;
+    numberOfPoints -=25;
+    points.textContent = numberOfPoints;
+
+
+    const ModalPromise = new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            resolve(currentRotation);
+        }, 3100);
+    }
+    );
+
+    ModalPromise.then((deg) => {
+        if(deg>360)
+        {
+            deg = deg%360;
+        }
+
+    });
+
+
+}
+
+
+let spinWheelButton = document.getElementById("SpinButton");
+spinWheelButton.addEventListener("click", speenTheWheel);
 }
 
 export default App;
